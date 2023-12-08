6bfad7ab3513d0c4c6d47ee6c13a4d9ba775fc64
Aleksandr Katolikov <akatolikov@ab-technology.ru>
2023-12-08 14:47:31 +0600

    change paypal to own cart

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import (
     request
 )
 
-import sendgrid
+# import sendgrid
 from datetime import date
 
 # App setup
@@ -20,11 +20,11 @@ app.config["SECRET_KEY"] = "some_really_long_random_string_here"
 
 # Get details for sendgrid details
 sendgrid_file = "sendgrid.txt"
-sendgrid_details = []
+# sendgrid_details = []
 
-with open(sendgrid_file) as f:
-    sendgrid_details = f.readlines()
-    sendgrid_details = [x.strip("\n") for x in sendgrid_details]
+# with open(sendgrid_file) as f:
+#     sendgrid_details = f.readlines()
+#     sendgrid_details = [x.strip("\n") for x in sendgrid_details]
 
 # Global Variables
 products_info = [
@@ -34,7 +34,8 @@ products_info = [
         "img": "shirt-101.jpg",
         "price": 18,
         "paypal": "LNRBY7XSXS5PA",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0,
     },
 
     {
@@ -43,7 +44,8 @@ products_info = [
         "img": "shirt-102.jpg",
         "price": 20,
         "paypal": "XP8KRXHEXMQ4J",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -52,7 +54,8 @@ products_info = [
         "img": "shirt-103.jpg",
         "price": 20,
         "paypal": "95C659J3VZGNJ",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -61,7 +64,8 @@ products_info = [
         "img": "shirt-104.jpg",
         "price": 18,
         "paypal": "Z5EY4SJN64SLU",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -70,7 +74,8 @@ products_info = [
         "img": "shirt-105.jpg",
         "price": 25,
         "paypal": "RYAGP5EWG4V4G",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -79,7 +84,8 @@ products_info = [
         "img": "shirt-106.jpg",
         "price": 20,
         "paypal": "QYHDD4N4SMUKN",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -88,7 +94,8 @@ products_info = [
         "img": "shirt-107.jpg",
         "price": 20,
         "paypal": "RSDD7RPZFPQTQ",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     },
 
     {
@@ -97,9 +104,12 @@ products_info = [
         "img": "shirt-108.jpg",
         "price": 25,
         "paypal": "LFRHBPYZKHV4Y",
-        "sizes": ["Small", "Medium", "Large"]
+        "sizes": ["Small", "Medium", "Large"],
+        "bought" : 0
     }
 ]
+
+bought_products = []
 
 # Functions
 
@@ -139,6 +149,40 @@ def get_list_view_html(product):
 
     return output
 
+def get_list_view_html_cart(product):
+    """Function to return html for given shirt
+
+    The product argument should be a dictionary in this structure:
+    {
+        "id": "shirt_id",
+        "name": "name_of_shirt",
+        "img": "image_name.jpg",
+        "price": price_of_shirt_as_int_or_flat,
+        "paypal": "paypal_id"
+        "sizes": ["array_of_sizes"]
+    }
+
+    The html is returned in this structure:
+    <li>
+      <a href="shirt/shirt_id">
+        <img src="/static/shirt_img" alt="shirt_name">
+        <p>View Details</p>
+      </a>
+    </li>
+    """
+    output = ""
+    image_url = url_for("static", filename=product["img"])
+    shirt_url = url_for("shirt", product_id=product["id"])
+    output = output + "<li>"
+    output = output + '<a href="' + shirt_url + '">'
+    output = (
+        output + '<img src="' + image_url +
+        '" al  t="' + product["name"] + '">')
+    output = output + "<p>" + str(product["bought"] * product["price"]) + "$ for " + str(product["bought"]) + " shirt(s)</p>"
+    output = output + "</a>"
+    output = output + "</li>"
+
+    return output
 
 # Routes
 # All functions should have a page_title variables if they render templates
@@ -156,7 +200,7 @@ def index():
                 Markup(get_list_view_html(product))
             )
     context["product_data"] = Markup("".join(product_data))
-    flash("This site is a demo do not buy anything")
+    # flash("This site is a demo do not buy anything")
     return render_template("index.html", **context)
 
 
@@ -168,7 +212,7 @@ def shirts():
     for product in products_info:
         product_data.append(Markup(get_list_view_html(product)))
     context["product_data"] = Markup("".join(product_data))
-    flash("This site is a demo do not buy anything")
+    # flash("This site is a demo do not buy anything")
     return render_template("shirts.html", **context)
 
 
@@ -181,15 +225,39 @@ def shirt(product_id):
         if product["id"] == product_id:
             my_product = product
     context["product"] = my_product
-    flash("This site is a demo do not buy anything")
+    # flash("This site is a demo do not buy anything")
     return render_template("shirt.html", **context)
 
+@app.route("/shirt/<product_id>/buy", methods=['POST'])
+def buy_shirt(product_id):
+    print("You bought: ", product_id)
+    context = {"page_title": "Shirts 4 Mike", "current_year": date.today().year}
+    my_product = ""
+    for product in products_info:
+        if product["id"] == product_id:
+            my_product = product
+            product["bought"] += 1
+            if product not in bought_products:
+                bought_products.append(product)
+    context["product"] = my_product
+    flash("Successfully bought " + my_product["name"])
+    return render_template("shirt.html", **context)
 
-@app.route("/receipt")
-def receipt():
+@app.route("/cart")
+def cart():
     """Function to display receipt after purchase"""
     context = {"page_title": "Shirts 4 Mike", "current_year": date.today().year}
-    return render_template("receipt.html", **context)
+    product_data = []
+    for product in bought_products:
+        product_data.append(Markup(get_list_view_html_cart(product)))
+    context["product_data"] = Markup("".join(product_data))
+    return render_template("cart.html", **context)
+
+# @app.route("/receipt")
+# def receipt():
+#     """Function to display receipt after purchase"""
+#     context = {"page_title": "Shirts 4 Mike", "current_year": date.today().year}
+#     return render_template("receipt.html", **context)
 
 
 @app.route("/contact")
@@ -203,8 +271,8 @@ def contact():
 @app.route("/send", methods=['POST'])
 def send():
     """Function to send email using sendgrid API"""
-    sendgrid_object = sendgrid.SendGridClient(
-        sendgrid_details[0], sendgrid_details[1])
+    # sendgrid_object = sendgrid.SendGridClient(
+    #     sendgrid_details[0], sendgrid_details[1])
     message = sendgrid.Mail()
     sender = request.form["email"]
     subject = request.form["name"]
@@ -213,8 +281,8 @@ def send():
     message.set_from(sender)
     message.set_subject(subject)
     message.set_html(body)
-    sendgrid_object.send(message)
-    flash("Email sent.")
+    # sendgrid_object.send(message)
+    # flash("Email sent.")
     return redirect(url_for("contact"))
 
 
