af1b67cce8e249aca1abf0f5c355e9802ae66476
Каменева Наталья <kameneva.natalka@gmail.com>
2024-01-06 14:18:55 +0700

    Изменения

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Main file for Shirts4Mike
-
 # Import statement
 from flask import (
     Flask,
@@ -29,84 +27,275 @@ sendgrid_file = "sendgrid.txt"
 # Global Variables
 products_info = [
     {
-        "id": "101",
-        "name": "Logo Shirt, Red",
-        "img": "shirt-101.jpg",
-        "price": 18,
-        "paypal": "LNRBY7XSXS5PA",
-        "sizes": ["Small", "Medium", "Large"],
-        "bought" : 0,
-    },
-
-    {
-        "id": "102",
-        "name": "Mike the Frog Shirt, Black",
-        "img": "shirt-102.jpg",
-        "price": 20,
-        "paypal": "XP8KRXHEXMQ4J",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "1",
+        "name": "Bed, Beige",
+        "img": "bed.jpg",
+        "price": 25000,
+        "paypal": "LFRHBPYZKH4Y",
         "bought" : 0
     },
 
     {
-        "id": "103",
-        "name": "Mike the Frog Shirt, Blue",
-        "img": "shirt-103.jpg",
-        "price": 20,
-        "paypal": "95C659J3VZGNJ",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "2",
+        "name": "Armchair, Blue",
+        "img": "armchair.jpg",
+        "price": 2500,
+        "paypal": "FRHBPYZKH4Y",
         "bought" : 0
     },
 
     {
-        "id": "104",
-        "name": "Logo Shirt, Green",
-        "img": "shirt-104.jpg",
-        "price": 18,
-        "paypal": "Z5EY4SJN64SLU",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "3",
+        "name": "Armchair, White",
+        "img": "armchair2.jpg",
+        "price": 10000,
+        "paypal": "LFRHBPYZH4Y",
         "bought" : 0
     },
 
     {
-        "id": "105",
-        "name": "Mike the Frog Shirt, Yellow",
-        "img": "shirt-105.jpg",
-        "price": 25,
-        "paypal": "RYAGP5EWG4V4G",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "4",
+        "name": "Armchair, Blue",
+        "img": "armchair3.jpg",
+        "price": 15000,
+        "paypal": "LRHBPYZKH4Y",
         "bought" : 0
     },
 
     {
-        "id": "106",
-        "name": "Logo Shirt, Gray",
-        "img": "shirt-106.jpg",
-        "price": 20,
-        "paypal": "QYHDD4N4SMUKN",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "5",
+        "name": "Bed, Brown",
+        "img": "bed2.jpg",
+        "price": 20000,
+        "paypal": "LRHBPYZKPP4Y",
         "bought" : 0
     },
 
     {
-        "id": "107",
-        "name": "Logo Shirt, Teal",
-        "img": "shirt-107.jpg",
-        "price": 20,
-        "paypal": "RSDD7RPZFPQTQ",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "6",
+        "name": "Bed, Green",
+        "img": "bed3.jpg",
+        "price": 25000,
+        "paypal": "LRHBPYZKPR4Y",
         "bought" : 0
     },
 
     {
-        "id": "108",
-        "name": "Mike the Frog Shirt, Orange",
-        "img": "shirt-108.jpg",
-        "price": 25,
-        "paypal": "LFRHBPYZKHV4Y",
-        "sizes": ["Small", "Medium", "Large"],
+        "id": "7",
+        "name": "Bedside, White",
+        "img": "bedside.jpg",
+        "price": 2000,
+        "paypal": "LRHBSYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "8",
+        "name": "Bookcase, White",
+        "img": "bookcase.jpg",
+        "price": 7000,
+        "paypal": "LRFBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "9",
+        "name": "Case, White",
+        "img": "case.jpg",
+        "price": 3000,
+        "paypal": "LRHBDYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "10",
+        "name": "Case, White",
+        "img": "case2.jpg",
+        "price": 3000,
+        "paypal": "LRYBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "11",
+        "name": "Case, Gray",
+        "img": "case3.jpg",
+        "price": 3500,
+        "paypal": "LRHBPYZKAP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "12",
+        "name": "Chair, Gray",
+        "img": "chair.jpg",
+        "price": 1500,
+        "paypal": "LRHSPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "13",
+        "name": "Chest, Wooden",
+        "img": "chest.jpg",
+        "price": 4000,
+        "paypal": "LTHBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "14",
+        "name": "Chest, White",
+        "img": "chest2.jpg",
+        "price": 3500,
+        "paypal": "KRHBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "15",
+        "name": "Chest, White",
+        "img": "chest3.jpg",
+        "price": 4500,
+        "paypal": "LRHBPYZKMP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "16",
+        "name": "Dresser, White",
+        "img": "dresser.jpg",
+        "price": 9000,
+        "paypal": "LRHAPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "17",
+        "name": "Dresser, Wooden",
+        "img": "dresser2.jpg",
+        "price": 10000,
+        "paypal": "LRHBPYZKPP4K",
+        "bought" : 0
+    },
+
+    {
+        "id": "18",
+        "name": "Dresser, White",
+        "img": "dresser3.jpg",
+        "price": 15000,
+        "paypal": "LRHRDPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "19",
+        "name": "Dresser, White",
+        "img": "dresser4.jpg",
+        "price": 13000,
+        "paypal": "LRHBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "20",
+        "name": "Dresser, Wooden",
+        "img": "dresser5.jpg",
+        "price": 12000,
+        "paypal": "LRHBPYZKPP5Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "21",
+        "name": "Dresser, Wooden",
+        "img": "dresser6.jpg",
+        "price": 11000,
+        "paypal": "LRHBPYZKPP8Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "22",
+        "name": "Dresser, White",
+        "img": "dresser7.jpg",
+        "price": 12000,
+        "paypal": "LRHBPQZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "23",
+        "name": "Shelf, Blue",
+        "img": "shelf.jpg",
+        "price": 1500,
+        "paypal": "LRHBPYZKPP1Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "24",
+        "name": "Sofa, Beige",
+        "img": "sofa.jpg",
+        "price": 25000,
+        "paypal": "QRHBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "25",
+        "name": "Sofa, Beige",
+        "img": "sofa2.jpg",
+        "price": 35000,
+        "paypal": "QRHBPYZKPP7Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "26",
+        "name": "Sofa, Brown",
+        "img": "sofa3.jpg",
+        "price": 23000,
+        "paypal": "QRHBSYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "27",
+        "name": "Table, Wooden",
+        "img": "table.jpg",
+        "price": 10000,
+        "paypal": "QRHBPAZKPP5Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "28",
+        "name": "TVstand, Black",
+        "img": "TVstand.jpg",
+        "price": 18000,
+        "paypal": "QFHBPYZKPP4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "29",
+        "name": "TVstand, White-wooden",
+        "img": "TVstand2.jpg",
+        "price": 13000,
+        "paypal": "QRHBPYZKTT4Y",
+        "bought" : 0
+    },
+
+    {
+        "id": "30",
+        "name": "TVstand, Black",
+        "img": "TVstand3.jpg",
+        "price": 15000,
+        "paypal": "QRHBDYZKPP3Y",
         "bought" : 0
     }
+
 ]
 
 bought_products = []
@@ -124,7 +313,6 @@ def get_list_view_html(product):
         "img": "image_name.jpg",
         "price": price_of_shirt_as_int_or_flat,
         "paypal": "paypal_id"
-        "sizes": ["array_of_sizes"]
     }
 
     The html is returned in this structure:
@@ -159,7 +347,6 @@ def get_list_view_html_cart(product):
         "img": "image_name.jpg",
         "price": price_of_shirt_as_int_or_flat,
         "paypal": "paypal_id"
-        "sizes": ["array_of_sizes"]
     }
 
     The html is returned in this structure:
@@ -243,6 +430,8 @@ def buy_shirt(product_id):
     flash("Successfully bought " + my_product["name"])
     return render_template("shirt.html", **context)
 
+
+
 @app.route("/cart")
 def cart():
     """Function to display receipt after purchase"""
@@ -289,3 +478,57 @@ def send():
 # Run application
 if __name__ == "__main__":
     app.run(debug=True)
+
+# @lab7.route('/lab7/api', methods=['POST'])
+# def api():
+#     data = request.json
+#     global paid  # Используем глобальную переменную paid
+
+#     if data['method'] == 'get-price':
+#         return get_price(data['params'])
+    
+#     if data['method'] == 'pay':
+#         response = pay(data['params'])
+#         if not response.get("error"):
+#             paid = True  # Обновляем состояние оплаты после успешной оплаты
+#         return response
+    
+#     if data['method'] == 'refund':
+#         if paid:  # Проверяем состояние оплаты
+#             response = refund(data['params'])
+#             paid = False  # Обновляем состояние оплаты после успешного возврата
+#             return response
+#         else:
+#             return {"result": None, "error": "Оплата еще не выполнена"}
+    
+#     abort(400)
+
+# def get_price(params):
+#     return {'result': calculate_price(params), "error": None}
+
+
+# #    return price
+
+# # def pay(params):
+# #     card_num = params['card']
+# #     if len(card_num) != 16 or not str(card_num).isdigit():
+# #         return {"result": None, "error": "Неверный номер карты"} 
+
+# #     cvv = params['cvv']
+# #     if len(cvv) != 3 or not cvv.isdigit():
+# #         return {"result": None, "error": "Неверный номер CVV/CVC"}
+    
+# #     price = params['price']  # Используем переданную цену
+
+# #     return {"result": f'С карты {card_num} списано {price} руб', "error": None}
+
+# # def refund(params):
+# #     card_num = params['card']
+# #     if len(card_num) != 16 or not str(card_num).isdigit():
+# #         return {"result": None, "error": "Неверный номер карты"}
+
+# #     cvv = params['cvv']
+# #     if len(cvv) != 3 or not cvv.isdigit():
+# #         return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+# #     return {"result": f'Деньги возвращены на карту {card_num}', "error": None}
